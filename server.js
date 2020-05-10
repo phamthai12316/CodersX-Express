e2b7d8b48145a02e837a2711c0e6b284a623c6f9
Glitch (codersx-express-lession-2) <none>
2020-05-10 04:20:50 +0000

    🎲🎣 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+// https://expressjs.com/en/starter/basic-routing.html
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/todos', (request, res) => {
+  res.render('todos/index',{
+    todos: [
+      {work: "Đi chợ"},{work: "Nấu cơm"},{work: "Rửa bát"},{work: "Học code tại CodersX"}
+    ]
+  })
+})
+
+// listen for requests :)
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
